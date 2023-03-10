e293d3d60e11c352da4480a09fd98f1fcfc06824
RubenMerfer <alumno@localhost>
2023-03-10 09:38:34 +0100

    hacemos commit en el main

--- src/com/polonorte/Reno.java
+++ src/com/polonorte/Reno.java
@@ -1,6 +1,11 @@
 package com.polonorte;
 
-
+/**
+ * clase Reno.
+ * Lo que hace es describir detalladamente al reno y sus características
+ * @author alumno
+ *@version 1.0
+ */
 public class Reno {
 
     private String nombre;
@@ -22,14 +27,23 @@ public class Reno {
 		this.apodo = apodo;
 	}
 
-
+/**
+ * Este método hace volar a un reno
+ * @param direccion - La dirección en la que vuela el reno
+ * @return El chillido que emite el reno al volar
+ */
 	public String volar(String direccion){
         System.out.println("Reno.volar");
 
         return "mmmmmiiiiii chus chus peeeeerrrr";
     }
 
-    
+    /**
+     * Este método hace correr al reno
+     * 
+     * @deprecated NO USAR MÁS, mejor usar el método {@link #volar(String)}
+     * @param direccion La dirección en la que correr
+     */
     public void correr(String direccion){
         //TODO terminar
     }
