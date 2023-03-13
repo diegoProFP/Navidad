31882168ec113f4ab406e960735aafb9f6d503ee
sofihuang <distance75432@gmail.com>
2023-03-13 10:13:32 +0100

    sofia's message

--- src/com/polonorte/elfos/Sofia_elfo_th.java
+++ src/com/polonorte/elfos/Sofia_elfo_th.java
@@ -5,6 +5,8 @@ public class Sofia_elfo_th extends ElfoGenerico {
 	protected Sofia_elfo_th(String nombre, Enum seccionLaboral) {
 		super("sofia", SeccionLaboralElfo.VIDEOCONSOLAS);
 		// TODO Auto-generated constructor stub
+		
+		System.out.println("hola soy sofia");
 	}
 
 
